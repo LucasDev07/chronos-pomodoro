8da8a2009fa9df86213c8cfd6f66f53f54d9ec66
Lucas Vargas <lukeamfernandes@gmail.com>
2025-05-29 15:23:10 -0300

    Testando componentes

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Olá Mundo</h1>
+    <App />
   </StrictMode>,
 );
