767032698c15350f0101fabc8e8fea4fca8ff17c
Lucas Vargas <lukeamfernandes@gmail.com>
2025-06-03 18:39:34 -0300

    Atualizações do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 
 export function App() {
-  console.log('Oi');
   return (
     <>
-      <Heading attr={123} attr2='String'>
-        Olá Mundo
-      </Heading>
-      <Heading>Olá Mundo</Heading>
-      <Heading>Olá Mundo</Heading>
-      <p>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 }
