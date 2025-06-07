aa0ae299e5c2aae4ede661c9b828e0c9819a7c65
Lucas Vargas <lukeamfernandes@gmail.com>
2025-06-07 15:28:39 -0300

    Mais atualizações

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
+import { Home } from './pages/Home';
+
 import './styles/theme.css';
 import './styles/global.css';
-import { Container } from './components/Container';
-import { Heading } from './components/Heading';
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Heading>LOGO</Heading>
-      </Container>
-
-      <Container>
-        <Heading>MENU</Heading>
-      </Container>
-    </>
-  );
+  return <Home />;
 }
---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,78 @@
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from 'lucide-react';
+import styles from './styles.module.css';
+import { useState, useEffect } from 'react';
+
+type AvaliableTheme = 'dark' | 'light';
+
+export function Menu() {
+  const [theme, setTheme] = useState<AvaliableTheme>(() => {
+    const storageTheme =
+      (localStorage.getItem('theme') as AvaliableTheme) || 'dark';
+    return storageTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
+
+  function handleChangeTheme(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault; //Não segue o link
+
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]); //Executa apenas quando o valor de theme muda
+
+  return (
+    <nav className={styles.menu}>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ir para a home'
+        title='Ir para a home'
+      >
+        <HouseIcon />
+      </a>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ver histórico'
+        title='Ver histórico'
+      >
+        <HistoryIcon />
+      </a>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Configurações'
+        title='Configurações'
+      >
+        <SettingsIcon />
+      </a>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Mudar Tema'
+        title='Mudar tema'
+        onClick={handleChangeTheme}
+      >
+        {nextThemeIcon[theme]}
+      </a>
+    </nav>
+  );
+}
