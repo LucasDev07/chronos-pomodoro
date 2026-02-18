e821fc27134df731a8a30e826ea9ce482c9304bd
Lucas Vargas Fernandes <lukeamfernandes@gmail.com>
2026-02-18 14:29:23 -0300

    Criando componente de menu com links e ícones

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import { Heading } from "./components/Heading";
+import { Container } from "./components/Container";
+import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
 
 import "./styles/theme.css";
 import "./styles/global.css";
 
 export function App() {
-  console.log("Oi");
-
   return (
     <>
-      <Heading />
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam facere
-        distinctio voluptatibus placeat quas unde adipisci corporis iusto
-        consectetur, eos culpa cumque. Sunt mollitia commodi voluptatibus,
-        asperiores voluptate dolores neque.
-      </p>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
     </>
   );
 }
